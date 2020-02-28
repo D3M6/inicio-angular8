664b8096d1874170059e84e91f542f488117136d
D3M6 <demg.working@gmail.com>
2020-02-28 18:03:25 +0100

    Componente con lista ngFor ngIf ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './components/correo/correo.component';
+import { ListaCorreosComponent } from './components/lista-correos/lista-correos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CorreoComponent
+    CorreoComponent,
+    ListaCorreosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
